4334852f2af0898c95f143205082b53e3c66b812
Simon <seh500@hotmail.com>
2025-10-27 22:40:13 +0100

    Changed run.sh

--- cloudflare/run.sh
+++ cloudflare/run.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -e
 
 mkdir -p /data/cloudflared
 
-# Download cloudflared if not exists
+# Download cloudflared if it does not exist
 if [ ! -f /usr/local/bin/cloudflared ]; then
     curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -o /usr/local/bin/cloudflared
     chmod +x /usr/local/bin/cloudflared
